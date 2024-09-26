8ec4d752e845ee0cd39cf3d924637c38c94d61ce
VovaVoroncov <gamesvova803@gmail.com>
2024-09-26 20:11:31 +0300

    Initial commit

---
+++ src/main/java/ua/goit/goit12/Note.java
@@ -0,0 +1,14 @@
+package ua.goit.hw12;
+
+import lombok.Data;
+
+@Data
+public class Note {
+
+    private long id;
+
+    private String title;
+
+    private String content;
+
+}
---
+++ src/test/java/ua/goit/hw12/GoitJavaDevModule12HwSpringBootCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.goit.hw12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoitJavaDevModule12HwSpringBootCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
